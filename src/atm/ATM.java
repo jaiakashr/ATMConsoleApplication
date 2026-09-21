31ab4cc7113a360746520b952426717162803881
akash-r <akash-r@akash-Victus-by-HP-Gaming-Laptop-15>
2026-09-21 11:30:50 +0530

    create menu

--- src/atm/ATM.java
+++ src/atm/ATM.java
@@ -10,9 +10,40 @@ public class ATM {
 		this.balance = balance;
 	}
 	
+	public boolean verifyPin(int enteredPin) {
+		return enteredPin == pin;
+	}
+	
+	public void deposit(double amount) {
+		if(amount>0) {
+			balance = balance + amount;
+			System.out.println("Rs."+amount+" deposit Successfully. ");
+			System.out.println("Your current balance is "+balance+".");
+		}else {
+			System.out.println("Invalid deposit amount.");
+		}
+	}
+	
+	public void withdraw(double amount) {
+		if(amount<=0) {
+			System.out.println("Invalid amount ");
+		}
+		else if(amount<balance) {
+			balance = balance - amount;
+			System.out.println("Rs."+amount+" withdrawal successfully. ");
+			System.out.println("Your current balance is "+balance+".");
+		}else {
+			System.out.println("Insufficient Balance! Can't able to withdraw. ");
+		}
+	}
+	
 	public void checkBalance() {
 		System.out.println("Currrent balance Rs."+balance);;
 	}
+
+	
+	
+	
 	
 
 }
--- src/atm/Main.java
+++ src/atm/Main.java
@@ -1,7 +1,64 @@
 package atm;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+		ATM atm = new ATM(2203, 985);
+		
+		System.out.println("Enter your PIN :");
+		int enteredPin = scan.nextInt();
+		if(atm.verifyPin(enteredPin)) {
+			System.out.println("Login Successfully");
+			
+			int choice = 0;
+			
+			while(choice != 5) {
+				System.out.println();
+				System.out.println("=================================");
+				System.out.println("             ATM Menu            ");
+				System.out.println("=================================");
+				System.out.println();
+				System.out.println("1. Check Balance ");
+				System.out.println("2. Deposit Money ");
+				System.out.println("3. Withdraw Money");
+				System.out.println("4. Create PIN ");
+				System.out.println("5. Exit");
+				
+				System.out.println("Enter your Choice : ");
+				choice = scan.nextInt();
+				
+				switch(choice) {
+				case 1: 
+					atm.checkBalance();
+					break;
+				
+				case 2:
+					System.out.println("Enter the amount to deposit: ");
+					double depositAmount = scan.nextDouble();
+					atm.deposit(depositAmount);
+					break;
+					
+				case 3:
+					System.out.println("Enter thr money to withdraw: ");
+					double withdrawalAmount = scan.nextDouble();
+					atm.withdraw(withdrawalAmount);
+					break;
+					
+				case 4:
+					break;
+					
+				case 5:
+					System.out.println("Thank you using our ATM. ");
+					break;
+			}
+			}
+		}else {
+			System.out.println("Incorrect PIN");
+		}
+		
+		scan.close();
 	}
 }
